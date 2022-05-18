dad235369bcadbea6ef0d98c1cb1cdfd54d75a23
JosueFrancoB <gerardo@cbin.mx>
2022-05-17 20:04:37 -0500

    change category model

--- models/categoria.js
+++ models/categoria.js
@@ -4,7 +4,6 @@ const CategoriaSchema = Schema({
     nombre: {
         type: String,
         required: [true, 'El nombre es obligatorio'],
-        unique: true
     },
     img: {
         type: String
